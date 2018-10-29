56f412bfa2e67efc0ccb9f80c9cee118cd8c5188
ARKK-Abhi1 <arkk.abhi1@gmail.com>
2018-10-29 13:16:26 +0530

    removed some patterns, will add them later on with proper implementation

--- dao_pattern/Dao.java
+++
@@ -1,8 +0,0 @@
-import java.util.Optional;
-import java.util.List;
-public interface Dao<T> {
-  Optional<T> get(long id);
-  List<T> getAll();
-  void add(T t);
-  void delete(T t);
-}
--- factory_design_pattern/Shape.java
+++
@@ -1,7 +0,0 @@
-public interface Shape {
-  int RECTANGLE = 1;
-  int CIRCLE = 2;
-  void draw();
-  float area();
-  float perimeter();
-}
